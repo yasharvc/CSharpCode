641d003504e79906567caad7b1f47dd7a2a5b6b0
Yashar ALIABBASI <yashar.aliabbasi@resinasoft.com>
2021-07-01 00:46:43 +0300

    Make test case better and more complex

--- test/UnitTests/CodeParserTests/PatternSpliterTests.cs
+++ test/UnitTests/CodeParserTests/PatternSpliterTests.cs
@@ -23,6 +23,20 @@ namespace UnitTests.CodeParserTests
             Assert.Contains(res.Item1["using"], x => x.Contains("using System;"));
             Assert.Contains(res.Item1["namespace"], x => x.Contains("namespace"));
         }
+        [Fact]
+        public async void Should_Extract_namespace_Name_Part()
+        {
+            var code = await ReadResourceAsync("namespace1.txt");
+            var spliter = new PatternSpliter("`|`namespace``->{using}`namespace`->{namespace}`|`{``->{name}`{_}`->{body}");
+
+            var res = await spliter.Process(code, 0);
+
+            res.Item2.ShouldBeGreaterThan(2);
+            res.Item1.Count.ShouldBeGreaterThan(1);
+            Assert.Contains(res.Item1["using"], x => x.Contains("using System;"));
+            Assert.Contains(res.Item1["namespace"], x => x.Contains("namespace"));
+            Assert.Contains(res.Item1.Keys, x => x.Equals("name"));
+        }
 
         private async Task<string> ReadResourceAsync(string fileName)
         {
